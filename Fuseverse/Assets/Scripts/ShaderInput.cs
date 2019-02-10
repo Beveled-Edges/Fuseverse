de7d26087c079123ed4748c2fd24e6df42011888
Steven <NightAngel47@users.noreply.github.com>
2019-02-10 10:16:49 -0600

    Gas Gaint WIP
    closer

--- Fuseverse/Assets/Scripts/ShaderInput.cs
+++ Fuseverse/Assets/Scripts/ShaderInput.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShaderInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Material gasShader;
+    public Vector3 shaderOffset;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButton(0))
+        {
+            HandleInput();
+        }
+    }
+
+    private void HandleInput()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray, out hitInfo, 100f, 9))
+        {
+            Debug.DrawLine(transform.position, hitInfo.transform.position);
+
+            shaderOffset += hitInfo.point * Time.deltaTime;
+
+            gasShader.SetFloat("_Bands", shaderOffset.y);
+        }
     }
 }
